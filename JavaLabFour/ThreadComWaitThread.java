4a088489887554569c001cd2a1dbc602c8a886b5
Yogesh0077 <cresthayoogeeshh@gmail.com>
2025-01-28 10:49:24 +0545

    Add files via upload

---
+++ JavaLabFour/ThreadComWaitThread.java
@@ -0,0 +1,17 @@
+package labfour;
+
+public class ThreadComWaitThread extends Thread{
+    ThreadComSharedData data;
+
+    public ThreadComWaitThread(ThreadComSharedData data) {
+        this.data = data;
+    }
+
+    public void run() {
+        try {
+            data.waitForIncrement();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
